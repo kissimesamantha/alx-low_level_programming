8f8bc4a1d33a9ffec3dd93bc46c1a414ea83ad2d
kissimesamantha <bambisamy@gmail.com>
2022-07-01 11:44:05 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+
 /**
- *
  *  * main - Entry point
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-puts("\"programming is like building a multilangual puzzle");
-	    return (0);
-
-} 
+	puts("\"programming is like building a multilangual puzzle");
+	return (0);
+}
