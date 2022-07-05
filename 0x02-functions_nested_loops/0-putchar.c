68ed914d41cbb4b375178702086dfc6670bc078c
kissimesamantha <bambisamy@gmail.com>
2022-07-05 10:15:02 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a < k)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
