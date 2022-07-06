b70d580581b61d3d83509d18797efba592910388
kissimesamantha <bambisamy@gmail.com>
2022-07-05 19:14:37 -0700

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,8 @@
 /**
  * _abs - function that computes the absolute value of an integer
  *@a: integer input
- * return: absolute value of a
+ * 
+ * Return: absolute value of a
  */
 int _abs(int a)
 {
