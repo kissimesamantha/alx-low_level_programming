81353fd969d82cab889f78899673c874001394bd
kissimesamantha <bambisamy@gmail.com>
2022-07-05 17:07:55 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,8 +5,9 @@
  * Return: 1 if "c" is lowercase 0 otherwise
  */
 /*function check if c is a lowercase */
-int _islower(int c)
+int _islower(int )
 {
+	int c;
 
 	if (c >= 97 && c <= 122)
 		return (1);
