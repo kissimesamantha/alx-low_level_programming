3416d9a457ffe0d7784e9465d736ce4c1b30eaa9
kissimesamantha <bambisamy@gmail.com>
2022-07-06 11:27:05 -0700

    print hour of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(m / 10) + '0');
-			_putchar(m % 10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 
 		}
 
