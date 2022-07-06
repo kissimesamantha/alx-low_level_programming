789107bc54a854416442f91c068f18f02b1cee63
kissimesamantha <bambisamy@gmail.com>
2022-07-06 13:56:52 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: entered integer n1
+ * @n2: entered integer n2
+ * Return: result
+ */
+int add(int n1, int n2)
+{
+	int add;
+
+	add = n1 + n2;
+	_putchar(add)
+
+}
