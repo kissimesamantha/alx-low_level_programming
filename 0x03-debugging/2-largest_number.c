253209d93f25add1fc2831313ce2fee5d7f7dcb7
kissimesamantha <bambisamy@gmail.com>
2022-07-07 15:21:56 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	printf("enter the three numbers:");
+	scanf("%a %b %c", &a, &b, &c);
+
+	if (a >= b && a >= c && b >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && a >= c && b >= c)
 	{
 		largest = b;
 	}
