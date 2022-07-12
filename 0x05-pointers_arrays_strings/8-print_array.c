459a5b33b4765ba68dfc611adf041bae21ccabe0
kissimesamantha <bambisamy@gmail.com>
2022-07-12 09:36:29 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n-1; i++)
 	{
 		printf("%d, ", a[i]);
-		if (i == n)
-			printf("%s", " ");
+		if (i == n-1)
+			printf("%d", a[i]);
 
 	}
 	printf("\n");
