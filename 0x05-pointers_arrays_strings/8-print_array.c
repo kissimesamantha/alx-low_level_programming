af6474679bc3465bc3d658c2bd3971fdcd2a2af7
kissimesamantha <bambisamy@gmail.com>
2022-07-12 08:09:27 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar((a[i]) + 'o');
+		_putchar((a[i]) + '0');
 	if (a[i] < 0)
-		_putchar((-a[i]) + 'o');
+		_putchar((-a[i]) + '0');
 	}
 
 	_putchar('\n');
