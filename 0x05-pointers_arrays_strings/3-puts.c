c75837e63207546aa6895eb1f5167d59b9d8bd65
kissimesamantha <bambisamy@gmail.com>
2022-07-11 17:51:18 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - function that prints a string, followed by a new line, to stdout
+ *@*str: pointer variable of string
+ *@str: address of string
+ *return: nothing
+ */
+
+void _puts(char *str)
+{
+	char x = *str;
+
+	_putchar('x');
+}
