ba3d8cb5da2153ea2d917588ba51a4234debcde5
kissimesamantha <bambisamy@gmail.com>
2022-07-17 07:29:02 -0700

    print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = 1; a < size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 1; b < size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar(35);
 			}
