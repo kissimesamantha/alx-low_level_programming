3b1fbb48c987d4d36ac441b38abe7296357d4672
kissimesamantha <bambisamy@gmail.com>
2022-07-19 13:46:03 -0700

    strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,14 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) !='\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
 				break;
 		}
-			if (*(accept + j) == '\0')
+			if (accept[j] == '\0')
 				break;
 	}
 	return (i);
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strpbrk -function that searches a string for any of a set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return: a pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
