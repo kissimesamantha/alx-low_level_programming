9c04bd15999968c2066c0ce053f9fb1080e4877e
kissimesamantha <bambisamy@gmail.com>
2022-07-19 14:08:18 -0700

    strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,7 +16,7 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j] <= '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (*s);
+				return (s[i]);
 		}
 		if (accept[j] == '\0')
 			return ('\0');
