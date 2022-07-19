aad881a05d2028714557afffa767689bc4420af0
kissimesamantha <bambisamy@gmail.com>
2022-07-19 16:27:16 -0700

    strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		s1 = haystack;
 		s2 = needle;
-		while (s2 != '\0' && *haystack == *s2)
+		while (*haystack != '\0' && *s2 != '\0' && *haystack == *s2)
 		{
 			haystack++;
 			s2++;
