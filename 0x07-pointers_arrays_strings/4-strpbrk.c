c9758db695d00d4d073044c995d23514f4287682
kissimesamantha <bambisamy@gmail.com>
2022-07-19 14:01:50 -0700

    strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,4 +9,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s);
+		}
+		if (accept[j] == '\0')
+			return ('\0');
+	}
+	return ('\0');
+}
