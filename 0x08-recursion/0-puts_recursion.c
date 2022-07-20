eb2697f0652787f14d1db730cc8ec26bb4237c47
kissimesamantha <bambisamy@gmail.com>
2022-07-20 13:20:16 -0700

    print string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,10 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] > '\0')
+	if (*s > '\0')
 	{
 		return;
 	}
 		_putchar(*s);
-		_puts_recursion(*(s + 1));
+		_puts_recursion(s + 1);
 }
