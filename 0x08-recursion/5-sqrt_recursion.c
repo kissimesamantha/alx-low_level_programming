0ffd56e1e76102372767d7575e81e50b8bd0185c
kissimesamantha <bambisamy@gmail.com>
2022-07-21 12:21:57 -0700

    pow recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: number
+ * Return : natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+
+}
