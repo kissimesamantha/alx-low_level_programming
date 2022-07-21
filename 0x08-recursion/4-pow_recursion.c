eaa5115e7a4f4d18a4df499546ac60029d632a05
kissimesamantha <bambisamy@gmail.com>
2022-07-21 11:40:49 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,10 +9,14 @@
 
 int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+	{
+		return (1);
+	}
 	if (y < 0)
 	{
 		return (-1);
 	}
-	_pow_recursion(x, y);
-	return (x);
+	/*_pow_recursion(x, y);*/
+	return (_pow_recursion(x, y);
 }
