f53af7ad5212937cc94911a88f856a394e72f6ac
kissimesamantha <bambisamy@gmail.com>
2022-07-26 13:57:12 -0700

    create array with allacated memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars.initializes with a specific char
+ * @size:size of array to be created
+ * @c: is the specific character
+ * Return: pointer to an array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i; 
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#This is all about memory allocation 
+#0x0B. C - malloc, free
