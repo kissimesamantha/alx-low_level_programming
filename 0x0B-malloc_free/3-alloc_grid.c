88b7dc04eba928351f66453b7f3441d232b94636
kissimesamantha <bambisamy@gmail.com>
2022-07-26 21:31:03 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,9 +28,9 @@ int **alloc_grid(int width, int height)
 			for (j = i ; j > 0; j--)
 			{
 				free(a[j]);
-				free(a);
-			return (NULL);
 			}
+			free(a);
+			return (NULL);
 		}
 	}
 	for (n = 0; n < height; n++)
