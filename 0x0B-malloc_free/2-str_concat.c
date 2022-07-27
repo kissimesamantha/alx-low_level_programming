14babba5304c68b11c9ce1200398014398282a36
kissimesamantha <bambisamy@gmail.com>
2022-07-26 20:40:43 -0700

    concatanate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,11 +23,13 @@ char *str_concat(char *s1, char *s2)
 
 	m = i + j;
 	s = (char *)malloc(m * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
 	n = 0;
 	while (n < m)
 	{
 		if (n < i)
-			s[n] = s1[i];
+			s[n] = s1[n];
 		if (n >= i)
 		{
 			s[n] = s2[j];
