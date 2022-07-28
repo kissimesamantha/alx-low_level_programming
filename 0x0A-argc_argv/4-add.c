c64e19ce027c0f185e15ed103741515bb076e35e
kissimesamantha <bambisamy@gmail.com>
2022-07-27 17:07:20 -0700

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stri,g.h>
+
+/**
+ * main -  program that adds positive numbers.
+ * If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits, print Error
+ * numbers and the addition of all the numbers can be stored in an int
+ * @argc: argument additional number
+ * @argv: argument vector
+ * Return : always 0(success) 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
+			{
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+	}
+	for (i =1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
