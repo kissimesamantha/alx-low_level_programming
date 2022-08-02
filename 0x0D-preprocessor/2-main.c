96e16c8bc07191c5ae5655689b8f15ff7951c634
kissimesamantha <bambisamy@gmail.com>
2022-08-02 16:31:56 -0700

    prints file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * main - program that prints the name of the file it was compiled from
  *followed by a new line.
- *@argc:contain total number of arguments
- *@argv: name of file
  *Return: 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-	printf("\n");
+	printf("%s\n", __FILE__);
 	return (0);
 }
