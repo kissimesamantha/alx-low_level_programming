5b66c3531954c8fa01eedd1e8a534a0973848244
kissimesamantha <bambisamy@gmail.com>
2022-08-16 12:03:50 -0700

    hot fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,15 +5,15 @@
  * @h: head of the list
  *Return: the number of elements
  */
- size_t listint_len(const listint_t *h)
- {
-	 const listint_t *cursor = h;
-	 size_t count = 0;
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-	 while (cursor != NULL)
-	 {
-		 count += 1;
-		 cursor = cursor->next;
-	 }
-	 return (count);
- }
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
