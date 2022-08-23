42a1c2b1be2f167a1145cad490cd9a95eb928aac
kissimesamantha <bambisamy@gmail.com>
2022-08-22 20:10:39 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if (filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
