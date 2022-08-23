9a27f167b22af9df55559f8821dcf0413a058013
kissimesamantha <bambisamy@gmail.com>
2022-08-22 21:13:55 -0700

    error file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
  * error_file - checks if files can be opened
  * @file_from: file_ from
  * @file_to: file to
+ * @argv: argument vector
  * Return: nothing
  */
 
@@ -34,7 +35,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, rwr;
-	char buff[1024];
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -49,12 +50,19 @@ int main(int argc, char *argv[])
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchhars = read(file_from, buff, 1024);
+		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
 		rwr = write(file_to, buf, nchars);
 		if (rwr == -1)
 			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
 	}
 
 	err_close = close(file_to);
