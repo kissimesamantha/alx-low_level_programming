e3f2d2dfeabca21d89b5ea41d9fa68cfaa99a453
kissimesamantha <bambisamy@gmail.com>
2022-08-22 19:55:26 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * create_file -creates a file
+ * @filename: filename
+ * @text_content: content writed in the file
+ * Return: 1 if it sucess, -1 f it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nbletters;
+	int rwr;
+
+	if (filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nbletters = 0; text_content[nbletters]; nbletters++)
+		;
+
+	rwr = write(fd, text_content, nbletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
